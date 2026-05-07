a99fa8e882cf38aa4fd76075b2f61f172a28a2c3
juliavozi <julia.voziniak.silva@escola.pr.gov.br>
2026-05-07 15:12:23 +0000

    aula 4

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-05-31T00:00:00");
+
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+
     }
 
     botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+
   };
 }
+contadores[0].textContent = "Contagem regressiva";
+let tempoAtual = new Date();
